7417d5882c8ee56b79fed58f850eb96a0635fc4e
Slava Pascaru <pascaruv@gmail.com>
2018-04-23 07:18:49 +0300

    My First Commit for the project FrontCoder

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+'use strict';
+
+//variable declaration 
+var gulp = require ('gulp'); 
+var gp   = require ('gulp-load-plugins')(); 
+var browserSync = require('browser-sync').create();
+
+
+
+// Static server
+gulp.task('serve', function() { // переименуем browser-sync в serve
+    browserSync.init({
+        server: {
+            baseDir: "./build"  // здесь добавляем откуда он должен взять файлы        }
+            }
+        });
+browserSync.watch('build', browserSync.reload)  // добовляем эту строку сцелью чтобы browserSync отслеживал папку build и когда там будут изменения он перезагружал браузер
+});
+
+
+//pug task
+gulp.task('pug', function(){
+    return gulp.src('src/pug/pages/*.pug')
+        .pipe(gp.pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('build'));
+ })
+
+//stylus task
+gulp.task('stylus', function(){
+    return gulp.src('src/static/stylus/*.styl')
+        .pipe(gp.sourcemaps.init())
+        .pipe(gp.stylus({}))
+        .pipe(gp.autoprefixer({
+            browsers: ['last 10 versions']
+        }))
+        .on("error",gp.notify.onError({
+            title: "style"
+        }))
+        .pipe(gp.csso())
+        .pipe(gp.sourcemaps.write())
+        .pipe(gulp.dest('build/static/css'));
+});
+
+
+
+gulp.task('watch', function(){
+    gulp.watch('src/pug/**/*.pug',gulp.series('pug'))           
+    gulp.watch('src/static/**/*.styl',gulp.series('stylus'));
+})
+
+gulp.task('default', gulp.series(
+    gulp.parallel('pug','stylus'),    // запускаем паралельно/одновременно
+    gulp.parallel('watch','serve')    // запускаем паралельно/одновременно
+));
+    
