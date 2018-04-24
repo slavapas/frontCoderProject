a45c16e7402f66d7250e1c4454f74d132191b23d
Slava Pascaru <pascaruv@gmail.com>
2018-04-24 07:21:59 +0300

    9th added js script, connected jQuery and slickSlider

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,5 +2,6 @@ module.exports = function(){
     $.gulp.task('watch', function(){
         $.gulp.watch('src/pug/**/*.pug',$.gulp.series('pug'))           
         $.gulp.watch('src/static/**/*.styl',$.gulp.series('stylus'));
+        $.gulp.watch('src/static/js/*.js',$.gulp.series('scripts'));
     });
 }
